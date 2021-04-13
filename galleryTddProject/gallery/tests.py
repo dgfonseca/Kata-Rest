c498352d9aabfa2426005dc212eaba46f7edad6f
David Fonseca <dg.fonseca@uniandes.edu.co>
2021-04-12 21:06:07 -0500

    Prueba 3 Test agregado pero falla por logica ROJO

--- galleryTddProject/gallery/tests.py
+++ galleryTddProject/gallery/tests.py
@@ -21,4 +21,10 @@ class GalleryTestCase(TestCase):
         current_data = json.loads(response.content)
         self.assertEqual(len(current_data), 2)
 
+    def test_add_user(self):
+        response = self.client.post('/gallery/addUser/', json.dumps(
+            {"username": "testUser", "first_name": "Test", "last_name": "User", "password": "AnyPas#5",
+             "email": "test@test.com"}), content_type='application/json')
+        current_data = json.loads(response.content)
+        self.assertEqual(current_data[0], 'testUser')
 
